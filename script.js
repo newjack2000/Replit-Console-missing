6fc048f3a4a8dd1e629a0d4a68054bbab4ac8ca8
Ronald Baker <83313552+newjack2000@users.noreply.github.com>
2022-09-19 01:26:15 +0000

    Added {to the last else statement - rb

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-// this is the code before the update
+function playGuessingGame(x, y) {
+  
+  let promptText = "Enter a number between 1 and 100."
 
+  //for loop. Checks to see if the user doesn't enter anything
+  //or if the user enters a number that is not a number
+  
+  numToGuess = x;
+  totalGuesses = y;
+
+  for (let guesses = 1; guesses < totalGuesses; guesses++) {
+    //check if the user doesn't enter anything.  If they do not enter anything, exist
+    let userGuess = prompt(promptText);
+
+    if (userGuess === null) {
+      return 0;
+    }
+
+    // checks to see if the value they entered is a number
+    else if (userGuess === "" || isNaN(userGuess)) {
+      promptText = "Please enter a number";
+      guesses--;
+    } else if (userGuess > numToGuess) {
+      guesses++;
+      promptText = "Please enter a number";
+      userGuess = prompt(userGuess + " is too large. Guess a smaller number.");
+    } else if (userGuess < numToGuess) {
+      guesses++;
+      promptText = "Please enter a number";
+      userGuess = prompt(userGuess + " is too small. Guess a smaller number.");
+    }  else {
+      promptText = "This is not working correctly";
+    }
+
+    return guesses;
+  }
+  }
+
+  var randomNum = Math.random() * 101 | 0,
+    totalGuesses = 10;
+
+  console.log("The random number is: " + randomNum);
+  // totalGuesses = prompt("How many guesses would you like to make?")
+
+  playGuessingGame(randomNum, totalGuesses);
+
+// this is the code before the update
+/*
 function playGuessingGame(x, y) {
 
   let numToGuess = x;
@@ -46,3 +92,4 @@ function playGuessingGame(x, y) {
   totalGuesses = prompt("How many guesses would you like to make?")
 
   playGuessingGame(randomNum, totalGuesses);
+*/
