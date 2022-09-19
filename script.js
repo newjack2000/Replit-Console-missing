51e7592759ea3092b1d7934919065b6f01aa5c61
Ronald Baker <83313552+newjack2000@users.noreply.github.com>
2022-09-19 04:08:31 +0000

    added guess count print statements - rb

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-function playGuessingGame(numToGuess, totalGuesses = 10){
-let promptText = "Enter a number between 1 and 100."
+function playGuessingGame(numToGuess, totalGuesses = 10) {
+  let promptText = "Enter a number between 1 and 100."
 
-// this section will be fo the loop.
+  // this section will be fo the loop.
 
-for(let guesses = 1; guesses < totalGuesses; guesses++) {
-let num = prompt(promptText)
+  for (let guesses = 1; guesses < totalGuesses; guesses++) {
+    let num = prompt(promptText)
 
-// check if the user does not enter anything and exists if nothing is entered
+    // check if the user does not enter anything and exists if nothing is entered
 
-if (num == null) {
-return 0
-}
+    if (num == null) {
+      console.log("The guess count is: " + guesses)
+      return 0
+    }
 
-// checks to see if the value the enter is a number
+    // checks to see if the value the enter is a number
 
-else if (num == "" || isNaN(num)){
-promptText = "Please enter a number:"
-guesses = guesses - 1
-}
+    else if (num == "" || isNaN(num)) {
+      promptText = "Please enter a number:"
+      console.log("The guess count is: " + guesses)
+      guesses = guesses - 1
+    }
 
-else if (num === numToGuess) {
-return guesses
-}
+    else if (num === numToGuess) {
+      console.log("The guess count is: " + guesses)
+      return guesses
+    }
 
-else if (num < numToGuess) {
-  console.log(num + " is too small.")
-  //promptText =  "Guess a larger number."
-  //num = prompt(promptText)
-  return guesses
-}
+    else if (num < numToGuess) {
+      console.log(num + " is too small.")
+      console.log("The guess count is: " + guesses)
+      //promptText =  "Guess a larger number."
+      //num = prompt(promptText)
+      return guesses
+    }
 
-else if (num > numToGuess) {
-  console.log(num + " is too large.")
-  //promptText =  "Guess a smaller number."
-  //num = prompt(promptText)
-  return guesses
+    else if (num > numToGuess) {
+      console.log(num + " is too large.")
+      console.log("The guess count is: " + guesses)
+      //promptText =  "Guess a smaller number."
+      //num = prompt(promptText)
+      return guesses
+    }
+
+
+    return;
   }
-
-
-return;
-}
 }
